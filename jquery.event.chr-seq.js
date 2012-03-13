9d80f45d3e86df3b4455764a3365c7f1f8449439
Deg Valentine <degvalentine@gmail.com>
2012-03-13 14:18:44 -0300

    Limit events to one per element per sequence. Now "event.stopPropagation()" can be used to implement default event handlers.

--- jquery.event.chr-seq.js
+++ jquery.event.chr-seq.js
@@ -12,13 +12,14 @@
 $.fn.charSequence = function(start, end, handler) {
 
     $(this).each(function(){
-        bind($(this), convert(start), convert(end), handler, getId($(this)));
+        bind($(this), convert(start), convert(end), handler);
     });
     return this;
 
     /**
      * Converts a character sequence to an array of character codes.
-     * @param seq string|[] the sequence to convert
+     * @param seq string the sequence to convert
+     * @return int[] character code array
      */
     function convert(seq)
     {
@@ -35,60 +36,64 @@ $.fn.charSequence = function(start, end, handler) {
     /**
      * Dispatches to method or initializes widget.
      */
-    function bind(elem, start, end, handler, eid)
+    function bind(elem, start, end, handler)
     {
-        // define data keys
-        var cacheKey = 'cache-' + eid;
-        var isCaching = 'isCaching-' + eid;
-        var eventKey = 'complex-keypress-' + eid;
-        var stateKey = 'state-' + eid;
-
-        // bind handler
-        elem.bind(eventKey, handler);
+        var eid = getId(elem, start, end);
         
         // bind listener to trigger handler
-        elem.data(isCaching, start.length == 0);
-        elem.data(stateKey, -1);
-        elem.data(cacheKey, '');
-        elem.bind('keypress', function(e) {
-            var state = elem.data(stateKey);
-
-            // starting sequence found, buffer data
-            if (elem.data(isCaching)) {
-                var cache = elem.data(cacheKey);
-                cache += String.fromCharCode(e.which);
-                elem.data(cacheKey, cache);
-                
-                if (end[state+1] != e.which) {
-                    state = -1;
-                }
-                if (end[state+1] == e.which) {
-                    ++state;
-                    if (state == end.length - 1) {
-                        elem.data(isCaching, start.length == 0);
-                        state = -1;
-                        elem.data(cacheKey, '');
-                        elem.trigger(eventKey, [cache.substr(0, cache.length - end.length)]);
-                    }
-                }
-                elem.data(stateKey, state);
-
-
-            // look for starting sequence
-            } else {
-                if (start[state+1] != e.which) {
-                    state = -1;
-                }
-                if (start[state+1] == e.which) {
-                    ++state;
-                    if (state == start.length - 1) {
-                        elem.data(isCaching, true);
+        if (elem.data('complex-keypress-id') == undefined) {
+            
+            var cacheKey = 'cache-' + eid;
+            var isCaching = 'isCaching-' + eid;
+            var eventKey = 'complex-keypress-' + eid;
+            var stateKey = 'state-' + eid;
+    
+            elem.data(isCaching, start.length == 0);
+            elem.data(stateKey, -1);
+            elem.data(cacheKey, '');
+            elem.bind('keypress', function(e) {
+                var state = elem.data(stateKey);
+    
+                // starting sequence found, buffer data
+                if (elem.data(isCaching)) {
+                    var cache = elem.data(cacheKey);
+                    cache += String.fromCharCode(e.which);
+                    elem.data(cacheKey, cache);
+                    
+                    if (end[state+1] != e.which) {
                         state = -1;
                     }
+                    if (end[state+1] == e.which) {
+                        ++state;
+                        if (state == end.length - 1) {
+                            elem.data(isCaching, start.length == 0);
+                            state = -1;
+                            elem.data(cacheKey, '');
+                            elem.trigger(eventKey, [cache.substr(0, cache.length - end.length)]);
+                        }
+                    }
+                    elem.data(stateKey, state);
+    
+    
+                // look for starting sequence
+                } else {
+                    if (start[state+1] != e.which) {
+                        state = -1;
+                    }
+                    if (start[state+1] == e.which) {
+                        ++state;
+                        if (state == start.length - 1) {
+                            elem.data(isCaching, true);
+                            state = -1;
+                        }
+                    }
+                    elem.data(stateKey, state);
                 }
-                elem.data(stateKey, state);
-            }
-        });
+            });
+        }
+        
+        // bind handler
+        elem.bind(eventKey, handler);
 
         return elem;
     }
@@ -96,15 +101,21 @@ $.fn.charSequence = function(start, end, handler) {
     /**
      * Returns an id to distinguish this complex keypress event.
      * @param elem the element to get id for
+     * @param start
+     * @param end
+     * @return string
      */
-    function getId(elem)
+    function getId(elem, start, end)
     {
-        id = elem.data('ckeid');
+        var id = elem.data('complex-keypress-id');
         if (!id) {
-            id = 0;
+            if (!this.ckeid) {
+                this.ckeid = 0;
+            }
+            id = ++ckeid;
+            elem.data('complex-keypress-id', id);
         }
-        elem.data('ckeid', id+1);
-        return id;
+        return id + '-' + start.join('l') + '-' + end.join('l');
     }
 
 }})(jQuery);
